20dd1e99b02e4d6f32635e0a444bee9cc47f5ab6
Ross Santos <rsantos@udel.edu>
2024-04-28 21:06:58 -0400

    Adding arg parser

--- run.py
+++ run.py
@@ -1,13 +1,27 @@
 import sys
+import argparse
 
 from common.logger import Logger
 from gui.recommender_gui import RecommenderGui
 from PyQt5.QtWidgets import QApplication
 
 
+def _parse_args():
+    parser = argparse.ArgumentParser(description="Recommender Application")
+    parser.add_argument(
+        "-l", "--log-level",
+        choices=["debug", "info", "warning", "error", "critical"],
+        default="debug",
+        help="Set the logger level (default: DEBUG)",
+    )
+    return parser.parse_args()
+
+
 def main():
+    args = _parse_args()
+    log_level = getattr(Logger, args.log_level.upper())
     app = QApplication(sys.argv)
-    window = RecommenderGui(Logger.DEBUG)
+    window = RecommenderGui(log_level)
     window.show()
     sys.exit(app.exec_())
 
