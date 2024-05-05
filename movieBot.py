e3c2f3e40148300e6a647a6d0f3c830e1f1ba404
Akanksha Singh <akanksha.singh0710@gmail.com>
2024-05-05 19:58:26 -0400

    Update movieBot.py

--- movieBot.py
+++ movieBot.py
@@ -22,7 +22,7 @@ words = pickle.load( open('words.pkl', 'rb'))
 classes = pickle.load( open('classes.pkl', 'rb'))
 model = keras.models.load_model('chatbot_model.h5')
 
-@st.cache_data
+@st.cache_resource
 def render_left_ui():
     return {
         'comedy': None,
@@ -94,13 +94,13 @@ def run():
             if movie_list:
                 st.text(movie_list)
 
-@st.cache_data
+@st.cache_resource
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
     return sentence_words
 
-@st.cache_data
+@st.cache_resource
 def bag_of_words(sentence):
     sentence_words = clean_up_sentence(sentence)
     bag = [0] * len(words)
@@ -110,7 +110,7 @@ def bag_of_words(sentence):
                 bag[i] = 1
     return np.array(bag)
 
-@st.cache_data
+@st.cache_resource
 def predict_class(sentence):
     bow = bag_of_words(sentence)
     res = model.predict(np.array([bow]))[0]
@@ -123,7 +123,7 @@ def predict_class(sentence):
         return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
     return return_list
 
-@st.cache_data
+@st.cache_resource
 def get_response(intents_list, intents_json, left_ui):
     try:
         tag = intents_list[0]['intent']
